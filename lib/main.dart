2712434cacb3b4f30ff8bbdfcbb9e94594d81ede
s4llm4n <s4llm4n@gmail.com>
2023-09-21 11:29:19 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firstproject/constant.dart';
+import 'package:firstproject/controllers/auth_controller.dart';
+import 'package:firstproject/views/screens/auth/login_screen.dart';
+import 'package:firstproject/views/screens/auth/signup_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then(((value) {
+    Get.put(AuthController());
+  }));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TikTuck',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
